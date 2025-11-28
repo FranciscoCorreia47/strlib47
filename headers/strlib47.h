eec73b01a6b84d162fafc8ec37321af623cc3bb8
FranciscoCorreia47 <jfranciscosacorreia@gmail.com>
2025-11-27 23:31:13 -0100

    Merged test-fixes

--- headers/strlib47.h
+++ headers/strlib47.h
@@ -5,20 +5,6 @@
 #include "strlib47_utils.h"
 
 /**
-<<<<<<< HEAD
- * @file strlib47.h
- * @brief The main header file for strlib47
- * @author Francisco Correia, also known as Ninja47 (by no one)
-*/
-
-/**
- * @brief The oficial strlib47 string type
-*/
-typedef struct str47{
-    char* str;
-    uint64_t len; //=strlib47L_strlen(str);
-}str47;
-=======
  * @file The main header file for the library, this is the one you include in your project
  * @author Francisco Correia
  */
