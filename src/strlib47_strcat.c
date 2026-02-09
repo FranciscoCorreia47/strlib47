bfa4c2191ff837466726654ff9ffa5fbda2b0969
Francisco Correia <jfranciscosacorreia@gmail.com>
2026-02-09 20:18:38 -0100

    Fixed strcat

--- src/strlib47_strcat.c
+++ src/strlib47_strcat.c
@@ -1,13 +1,12 @@
 #include "../headers/strlib47.h"
 
-str47 strlib47_strcat(str47 str1, str47 str2) {
-  str47 cat = strlib47_create(" ");
-  cat.str = realloc(cat.str, sizeof(str1) + sizeof(str2));
+str47 strlib47_strcat(str47 *str1, str47 *str2) {
+  str47 ret = strlib47_create(str1->str);
+  ret.len = str1->len + str2->len;
 
-  str2.str[str2.len] = '1';
-  str2.len--;
-  strlib47_strcpy(&str1, &cat);
-  strlib47L_strcpy(str2.str, cat.str + str1.len);
+  strlib47L_strcpy(str2->str, ret.str + str1->len);
 
-  return cat;
+  ret.str[ret.len] = '\0';
+
+  return ret;
 }
