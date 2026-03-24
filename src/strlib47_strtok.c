0d730292435db7712303a6e1796ffca47cee47f9
Francisco Correia <jfranciscosacorreia@gmail.com>
2026-03-24 11:49:08 -0100

    strtok v1.4

--- src/strlib47_strtok.c
+++ src/strlib47_strtok.c
@@ -1,11 +1,25 @@
 #include "../headers/strlib47.h"
 
 void strlib47_strtok(str47 *src, char *tok, int64_t *saveptr) {
-  *saveptr =
-      !*saveptr ? strlib47_indexOf(tok, src->str)
-                : *saveptr + strlib47_indexOf(tok, &src->str[*saveptr + 1]) + 1;
+  int64_t idx1 = strlib47_indexOf(tok, src->str);
+  int64_t idx2 = strlib47_indexOf(tok, &src->str[*saveptr + 1]);
+
+  if (!*saveptr) {
+    if (!idx1)
+      *saveptr = idx2;
+    else
+      *saveptr = idx1;
+  } else {
+    if (idx2 != -1)
+      *saveptr += idx2 + 1;
+    else
+      *saveptr = idx2;
+  }
+
   if (*saveptr < 0)
     return;
   src->str[*saveptr] = '\0';
   src->len = *saveptr;
 }
+
+// ,hello,world
