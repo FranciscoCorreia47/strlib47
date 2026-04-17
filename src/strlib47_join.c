504fed4171d8ae5ab8a5d24fe4b4aaa3457d3b01
Francisco Correia <jfranciscosacorreia@gmail.com>
2026-04-17 14:34:49 -0100

    strlib47_join is functional

--- src/strlib47_join.c
+++ src/strlib47_join.c
@@ -2,14 +2,16 @@
 
 str47 strlib47_join(str47_arr *arr, char *sep) {
   str47 ret = strlib47_create("\0");
-  uint64_t sep_len = strlib47L_strlen(sep);
 
-  if (!arr || arr->len <= 0)
+  if (!arr || arr->len <= 0 || !sep)
     return ret;
+
+  uint64_t sep_len = strlib47L_strlen(sep);
 
   strlib47_strcpy(&arr->data[0], &ret);
   ret.str = strlib47L_strcat(ret.str, sep);
   ret.len += sep_len;
+
   for (uint64_t i = 1; i < arr->len - 1; ++i) {
     ret = strlib47_strcat(&ret, &arr->data[i]);
     ret.str = strlib47L_strcat(ret.str, sep);
